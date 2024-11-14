2b2661851e65f67b838ac25f9d0dbbed40ade8a3
Genilson Maceno <genilson_de_maceno@hotmail.com>
2024-11-14 16:28:09 -0300

    Added layers Presetation WebApi and delete folder Domain

--- src/Domain/Models/Base.cs
+++
@@ -1,7 +0,0 @@
-namespace DDDWebAPI.Domain.Models;
-
-public class Base
-{
-    public int? Id { get; set; }
-}
-
--- src/Domain/Models/Produto.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Models;
-public class Produto : Base
-{
-    public string Nome { get; set; }
-    public decimal Valor { get; set; }
-    public bool Disponivel { get; set; }
-    public int ClienteId { get; set; }
-    public virtual Cliente Cliente { get; set; }
-}
